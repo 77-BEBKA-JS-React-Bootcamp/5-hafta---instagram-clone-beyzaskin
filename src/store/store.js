cb0c1622e6c27bcf34bffa6ab212cc58fc4a657b
Beyza <beyzaskin123@gmail.com>
2021-03-25 03:24:50 +0200

    5-hafta---instagram-clone-beyzaskin

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/index";
+
+
+const store = createStore ( //from redux
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))  //from redux-thunk
+);
+
+export default store;
